489098635229ef6d589eaf55167ec35e188d5673
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-29 15:35:36 -0400

    Add custom enumerable methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,77 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    return self.dup unless block_given?
+    index = 0
+    for elem in self do
+      yield elem, index
+    index += 1
+    end
+  end
+
+  def my_select
+    return self.dup unless block_given?
+    result = []
+    for elem in self do
+      result << elem if yield(elem) #yield elem is true/false
+    end
+    result
+  end
+
+  def my_all?
+    result = true
+    for elem in self do
+      result = yield(elem) ? true : false
+    end
+    result
+  end
+
+  def my_any?
+    result = false
+    for elem in self do
+      if yield(elem)
+        result = true
+      end
+    end
+    result
+  end
+
+  def my_none?
+    result = true
+    for elem in self do
+      if yield(elem)
+        result = false
+      end
+    end
+    result
+  end
+
+  def my_count
+    return self.size unless block_given?
+    count = 0
+    for elem in self do
+      if yield(elem)
+        count += 1
+      end
+    end
+    count
+  end 
+
+  def my_map 
+    return self.dup unless block_given?
+    result = []
+    for elem in self do
+      result << yield(elem)
+    end
+    result
+  end
+
+  def my_inject(value)
+    for elem in self do
+      value = yield(value, elem)
+    end
+    value
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +80,11 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return self.dup unless block_given?
+    for elem in self do
+      yield elem
+    end
+    self
+  end
 end
