57155a883e24e0ab616db26ccef115759a883597
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-29 16:21:15 -0400

    Add hash tests for #my_count

--- spec/my_count_spec.rb
+++ spec/my_count_spec.rb
@@ -4,6 +4,7 @@ require_relative '../lib/my_enumerables'
 
 RSpec.describe Enumerable do
   subject(:enumerable) { [1, 1, 2, 3, 5, 8, 13, 21, 34] }
+  subject(:enumerable_hash) { {:one => 1, :two => 2} }
 
   describe '#my_count' do
     context 'when not given a block' do
@@ -17,6 +18,18 @@ RSpec.describe Enumerable do
         expect(enumerable.my_count { |value| value > 5 }).to eq 4
         expect(enumerable.my_count { |value| value <= 5 }).to eq 5
         expect(enumerable.my_count { |value| value == 5 }).to eq 1
+      end
+    end
+
+    context 'when applied to hash given a block' do
+      it 'returns count of elements that satisfy condition' do
+        expect(enumerable_hash.my_count { |k, v| v > 1 }).to eq 1
+      end
+    end
+
+    context 'when applied to hash given no block' do
+      it 'returns count of elements that satisfy condition' do
+        expect(enumerable_hash.my_count).to eq enumerable_hash.size
       end
     end
   end
